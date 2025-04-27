7ef78e319a31f1e2e939d4728a2f1b548d1a8f45
niranois13 <8784@holbertonstudents.com>
2025-04-27 10:47:29 +0200

    Task 0

--- Webpack/task_0/src/index.js
+++ Webpack/task_0/src/index.js
@@ -1,6 +1,6 @@
 const $ = require("jquery");
 
-$(document).ready(function () {
+$(function () {
   $("body").append("<p>Holberton Dashboard</p>");
   $("body").append("<p>Dashboard data for the students</p>");
   $("body").append("<p>Copyright - Holberton School</p>");
