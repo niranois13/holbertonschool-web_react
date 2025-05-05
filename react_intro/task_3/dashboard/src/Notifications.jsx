9ad0e14c06724f67910a4a7c39f66498fbcae922
niranois13 <8784@holbertonstudents.com>
2025-05-05 14:46:03 +0200

    Task 7

--- react_intro/task_3/dashboard/src/Notifications.jsx
+++ react_intro/task_3/dashboard/src/Notifications.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Notifications.css';
-import closeIcon from './assets/close-button.png';
+import closeIcon from './assets/close-icon.png';
 import { getLatestNotification } from './utils'; 
 
 export default function Notifications() {
