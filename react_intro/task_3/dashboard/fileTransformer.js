aa3e7e695e94370129620298e532b35e40d57928
niranois13 <8784@holbertonstudents.com>
2025-05-05 14:03:30 +0200

    Task 6

--- react_intro/task_3/dashboard/fileTransformer.js
+++ react_intro/task_3/dashboard/fileTransformer.js
@@ -2,7 +2,6 @@ import path from 'path';
 
 
 export default {
-    // eslint-disable-next-line no-unused-vars
     process(sourceText, sourcePath, options) {
         return {
             code: `module.exports = ${JSON.stringify(path.basename(sourcePath))};`,
