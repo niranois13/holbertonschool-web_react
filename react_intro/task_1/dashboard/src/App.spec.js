c4c7408cb3d1ab1cbd5e54fb34b8a8e8d54e4e91
niranois13 <8784@holbertonstudents.com>
2025-05-05 11:28:30 +0200

    Task 1

---
+++ react_intro/task_1/dashboard/src/App.spec.js
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App.jsx";
+
+test('renders an h1 with text "School dashboard"', () => {
+  render(<App />);
+  const heading = screen.getByRole('heading', { name: /School dashboard/i });
+  expect(heading).toBeInTheDocument();
+});
+
+test('renders correct text in App-body and App-footer', () => {
+  render(<App />);
+  expect(screen.getByText(/Login to access the full dashboard/i)).toBeInTheDocument();
+  expect(screen.getByText(/Copyright 2025 - holberton School/i)).toBeInTheDocument();
+});
+
+test('renders an img element', () => {
+  render(<App />);
+  const img = screen.getByRole('img', { name: /holberton logo/i });
+  expect(img).toBeInTheDocument();
+});
---
+++ react_intro/task_1/dashboard/src/Notifications.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Notifications() {
+    return (
+    <div className="notifications"> 
+        <p>Here is the list of notifications</p>
+    </div>
+    );
+}
+
+export default Notifications;
