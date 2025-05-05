e2e00a9e25a2a693d69e86bdf1280684080331cb
niranois13 <8784@holbertonstudents.com>
2025-05-05 13:18:42 +0200

    Task 5

--- react_intro/task_2/dashboard/src/Notifications.jsx
+++ react_intro/task_2/dashboard/src/Notifications.jsx
@@ -2,9 +2,40 @@ import React from 'react';
 
 function Notifications() {
     return (
-    <div className="notifications"> 
-        <p>Here is the list of notifications</p>
-    </div>
+        <div className="notifications">
+            <button
+                aria-label="Close"
+                onClick={handleClick}
+                style={{
+                    position: 'absolute',
+                    top: '10px',
+                    right: '10px',
+                    background: 'none',
+                    border: 'none',
+                    cursor: 'pointer'
+                }}
+            >
+                <img
+                    src={closeIcon}
+                    alt="close icon"
+                    style={{ width: '10px', height: '10px' }}
+                />
+
+            </button>
+
+            <p>Here is the list of notifications</p>
+
+            <ul>
+                <li data-priority="default">New course available</li>
+                <li data-priority="urgent">New resume available</li>
+                <li
+                    data-priority="urgent"
+                    dangerouslySetInnerHTML={
+                        { __html: getLatestNotification() }
+                    }
+                ></li>
+            </ul>
+        </div>
     );
 }
 
