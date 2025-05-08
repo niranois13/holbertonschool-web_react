01d435a6f8e69062a5ace0ee831b747c00c9ec1b
niranois13 <8784@holbertonstudents.com>
2025-05-08 10:51:10 +0200

    Task 4

--- react_props/task_4/dashboard/src/App/App.jsx
+++ react_props/task_4/dashboard/src/App/App.jsx
@@ -3,14 +3,24 @@ import Notifications from '../Notifications/Notifications';
 import Header from '../Header/Header';
 import Login from '../Login/Login';
 import Footer from '../Footer/Footer';
+import CourseList from '../CourseList/CourseList';
 import { getLatestNotification } from '../utils/utils'
 
 function App() {
+  const isLoggedIn = false;
+
   const notificationsList = [
     { id: Math.floor(Math.random() * Date.now()), type: 'default', value: 'New course available' },
     { id: Math.floor(Math.random() * Date.now()), type: 'urgent', value: 'New resume available' },
     { id: Math.floor(Math.random() * Date.now()), type: 'urgent', html: { __html: getLatestNotification() } },
   ];
+
+  const coursesList = [
+    { id: Math.floor(Math.random() * Date.now()), name: 'ES6', credit: 60 },
+    { id: Math.floor(Math.random() * Date.now()), name: 'Webpack', credit: 20 },
+    { id: Math.floor(Math.random() * Date.now()), name: 'React', credit: 40 },
+  ];
+
   return (
     <>
       <div className="root-notifications">
@@ -18,7 +28,7 @@ function App() {
       </div>
 
       <Header />
-      <Login />
+      {isLoggedIn ? <CourseList courses={coursesList} /> : <Login />}
       <Footer />
     </>
   )
