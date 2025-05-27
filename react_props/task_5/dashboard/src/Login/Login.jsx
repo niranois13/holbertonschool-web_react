843ef601336d2f902679ee7f02d331c87f88aa58
niranois13 <8784@holbertonstudents.com>
2025-05-27 15:13:27 +0200

    Task 5

--- react_props/task_5/dashboard/src/Login/Login.jsx
+++ react_props/task_5/dashboard/src/Login/Login.jsx
@@ -4,10 +4,10 @@ export default function Login() {
     return (
     <div className="App-body">
         <p>Login to access the full dashboard</p>
-        <label htmlFor="email">Email:
+        <label htmlFor="email">Email
             <input id="email" type="email" />
         </label>
-        <label htmlFor="password">Password:
+        <label htmlFor="password">Password
             <input id="password" type="password" />
         </label>
         <button>OK</button>
