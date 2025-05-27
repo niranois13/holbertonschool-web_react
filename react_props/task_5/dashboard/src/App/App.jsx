4567916b613131aa445c00aed5f8f402a7ba5a35
niranois13 <8784@holbertonstudents.com>
2025-05-27 14:56:20 +0200

    Task 5

--- react_props/task_5/dashboard/src/App/App.jsx
+++ react_props/task_5/dashboard/src/App/App.jsx
@@ -17,9 +17,9 @@ function App() {
   ];
 
   const coursesList = [
-    { id: 1, name: 'ES6', credit: 60 },
-    { id: 2, name: 'Webpack', credit: 20 },
-    { id: 3, name: 'React', credit: 40 },
+    { id: 1, name: 'ES6', credit: '60' },
+    { id: 2, name: 'Webpack', credit: '20' },
+    { id: 3, name: 'React', credit: '40' },
   ];
 
   return (
