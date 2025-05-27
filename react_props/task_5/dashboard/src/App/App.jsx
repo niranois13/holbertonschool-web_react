e6e30c3f6a32689b4f163248fd7eb9f196121472
niranois13 <8784@holbertonstudents.com>
2025-05-27 15:00:17 +0200

    Task 5

--- react_props/task_5/dashboard/src/App/App.jsx
+++ react_props/task_5/dashboard/src/App/App.jsx
@@ -8,34 +8,30 @@ import { getLatestNotification } from '../utils/utils'
 import React from 'react';
 
 function App() {
-  const isLoggedIn = true;
+  const isLoggedIn = false;
 
   const notificationsList = [
-    { id: 1, type: 'default', value: 'New course available' },
-    { id: 2, type: 'urgent', value: 'New resume available' },
-    { id: 3, type: 'urgent', html: { __html: getLatestNotification() } },
+    { id: Math.floor(Math.random() * Date.now()), type: 'default', value: 'New course available' },
+    { id: Math.floor(Math.random() * Date.now()), type: 'urgent', value: 'New resume available' },
+    { id: Math.floor(Math.random() * Date.now()), type: 'urgent', html: { __html: getLatestNotification() } },
   ];
 
   const coursesList = [
-    { id: 1, name: 'ES6', credit: '60' },
-    { id: 2, name: 'Webpack', credit: '20' },
-    { id: 3, name: 'React', credit: '40' },
+    { id: 1, name: 'ES6', credit: 60 },
+    { id: 2, name: 'Webpack', credit: 20 },
+    { id: 3, name: 'React', credit: 40 },
   ];
 
   return (
-    <>
+    <React.Fragment>
       <div className="root-notifications">
-        <Notifications notifications={notificationsList}/>
+        <Notifications notifications={notificationsList} displayDrawer={true}/>
       </div>
 
       <Header />
-      {isLoggedIn ? ( 
-        <CourseList courses={coursesList} />
-        ) : (
-        <Login />
-        )}
+      {isLoggedIn ? <CourseList courses={coursesList} /> : <Login />}
       <Footer />
-    </>
+    </React.Fragment>
   )
 }
 
