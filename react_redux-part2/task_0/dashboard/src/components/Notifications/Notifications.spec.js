549fc76e2c70bd31e3aaed7242b011923901380b
niranois13 <8784@holbertonstudents.com>
2025-06-13 15:53:17 +0200

    Task 0

--- react_redux-part2/task_0/dashboard/src/components/Notifications/Notifications.spec.js
+++ react_redux-part2/task_0/dashboard/src/components/Notifications/Notifications.spec.js
@@ -1,14 +1,12 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import { Provider } from "react-redux";
-import configureStore from "redux-mock-store";
-import thunk from "redux-thunk";
+import configureMockStore from "redux-mock-store"; 
 import Notifications from "./Notifications";
 import axios from "axios";
 
 jest.mock("axios");
 
-const middlewares = [thunk];
-const mockStore = configureStore(middlewares);
+const mockStore = configureMockStore([]);
 
 describe("Notifications component", () => {
   let store;
